cf9d0f3e2c3274061eb265fd8e7f770061f729ac
deraiser <deraiser@gmx.de>
2023-04-28 16:42:42 +0200

    Add the basic structure of the radio system.

---
+++ files_radio/global.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @author	Marco Daries
+ * @copyright	2023 Daries.dev
+ * @license	Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0) <https://creativecommons.org/licenses/by-nd/4.0/>
+ */
+// include config
+require_once(dirname(__FILE__) . '/app.config.inc.php');
+
+// include wcf
+require_once(RELATIVE_WCF_DIR . 'global.php');
