3a4443ad37ff10b3a9cc63e5eba78b1d661c9859
deraiser <deraiser@gmx.de>
2023-06-12 22:08:27 +0200

    add endpoint database

--- files_radio/acp/database/install_dev.daries.radio.php
+++ files_radio/acp/database/install_dev.daries.radio.php
@@ -12,6 +12,8 @@ use wcf\system\database\table\column\NotNullInt10DatabaseTableColumn;
 use wcf\system\database\table\column\NotNullVarchar255DatabaseTableColumn;
 use wcf\system\database\table\column\ObjectIdDatabaseTableColumn;
 use wcf\system\database\table\DatabaseTable;
+use wcf\system\database\table\index\DatabaseTableForeignKey;
+use wcf\system\database\table\index\DatabaseTableIndex;
 use wcf\system\database\table\index\DatabaseTablePrimaryIndex;
 
 return [
@@ -32,5 +34,33 @@ return [
         ->indices([
             DatabaseTablePrimaryIndex::create()
                 ->columns(['streamID']),
+        ]),
+    DatabaseTable::create('radio1_stream_endpoint')
+        ->columns([
+            ObjectIdDatabaseTableColumn::create('endpointID'),
+            NotNullInt10DatabaseTableColumn::create('streamID'),
+            NotNullVarchar255DatabaseTableColumn::create('name'),
+            NotNullVarchar255DatabaseTableColumn::create('path'),
+            MediumtextDatabaseTableColumn::create('config'),
+            DefaultFalseBooleanDatabaseTableColumn::create('isDefault'),
+            NotNullInt10DatabaseTableColumn::create('showOrder')
+                ->defaultValue(0),
+        ])
+        ->indices([
+            DatabaseTablePrimaryIndex::create()
+                ->columns(['endpointID']),
+            DatabaseTableIndex::create('name_streamID')
+                ->type(DatabaseTableIndex::UNIQUE_TYPE)
+                ->columns(['name', 'streamID']),
+            DatabaseTableIndex::create('path_streamID')
+                ->type(DatabaseTableIndex::UNIQUE_TYPE)
+                ->columns(['path', 'streamID']),
+        ])
+        ->foreignKeys([
+            DatabaseTableForeignKey::create()
+                ->columns(['streamID'])
+                ->referencedTable('radio1_stream')
+                ->referencedColumns(['streamID'])
+                ->onDelete('CASCADE'),
         ]),
 ];
